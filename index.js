e008082dd0be0889a300083881623ec4b2755d4f
Anugrah Lambogo <lambogoai@gmail.com>
2020-01-30 16:12:58 -0600

    fixed typos

--- index.js
+++ index.js
@@ -35,30 +35,17 @@ const generateShoppingItemsString = function (shoppingList) {
   return items.join('');
 };
 
-/**
- * Render the shopping list in the DOM
- */
+
 const render = function () {
-  // Set up a copy of the store's items in a local 
-  // variable 'items' that we will reassign to a new
-  // version if any filtering of the list occurs.
+
   let items = [...store.items];
-  // If the `hideCheckedItems` property is true, 
-  // then we want to reassign filteredItems to a 
-  // version where ONLY items with a "checked" 
-  // property of false are included.
+
   if (store.hideCheckedItems) {
     items = items.filter(item => !item.checked);
   }
 
-  /**
-   * At this point, all filtering work has been 
-   * done (or not done, if that's the current settings), 
-   * so we send our 'items' into our HTML generation function
-   */
   const shoppingListItemsString = generateShoppingItemsString(items);
 
-  // insert that HTML into the DOM
   $('.js-shopping-list').html(shoppingListItemsString);
 };
 
@@ -95,49 +82,32 @@ const getItemIdFromElement = function (item) {
     .data('item-id');
 };
 
-/**
- * Responsible for deleting a list item.
- * @param {string} id 
- */
+
 const deleteListItem = function (id) {
-  // As with 'addItemToShoppingLIst', this 
-  // function also has the side effect of
-  // mutating the global store value.
-  //
-  // First we find the index of the item with 
-  // the specified id using the native
-  // Array.prototype.findIndex() method. 
+
   const index = store.items.findIndex(item => item.id === id);
-  // Then we call `.splice` at the index of 
-  // the list item we want to remove, with 
-  // a removeCount of 1.
+
   store.items.splice(index, 1);
 };
 
 const handleDeleteItemClicked = function () {
-  // Like in `handleItemCheckClicked`, 
-  // we use event delegation.
+
   $('.js-shopping-list').on('click', '.js-item-delete', event => {
-    // Get the index of the item in store.items.
+
     const id = getItemIdFromElement(event.currentTarget);
-    // Delete the item.
+
     deleteListItem(id);
-    // Render the updated shopping list.
+
     render();
   });
 };
 
-/**
- * Toggles the store.hideCheckedItems property
- */
+
 const toggleCheckedItemsFilter = function () {
   store.hideCheckedItems = !store.hideCheckedItems;
 };
 
-/**
- * Places an event listener on the checkbox 
- * for hiding completed items.
- */
+
 const handleToggleFilterClick = function () {
   $('.js-filter-checked').click(() => {
     toggleCheckedItemsFilter();
@@ -145,15 +115,7 @@ const handleToggleFilterClick = function () {
   });
 };
 
-/**
- * This function will be our callback when the
- * page loads. It is responsible for initially 
- * rendering the shopping list, then calling 
- * our individual functions that handle new 
- * item submission and user clicks on the 
- * "check" and "delete" buttons for individual 
- * shopping list items.
- */
+
 const handleShoppingList = function () {
   render();
   handleNewItemSubmit();
@@ -162,5 +124,4 @@ const handleShoppingList = function () {
   handleToggleFilterClick();
 };
 
-// when the page loads, call `handleShoppingList`
 $(handleShoppingList);
